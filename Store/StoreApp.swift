e25a95dcb438f7eae7c55d7a9586336cb843d639
Rahmat Susanto <rarito.jogja@gmail.com>
2023-11-12 05:54:47 +0700

    Initial Commit

---
+++ Store/StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreApp.swift
+//  Store
+//
+//  Created by Rahmat Susanto on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
